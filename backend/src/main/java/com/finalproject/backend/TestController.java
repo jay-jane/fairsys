c376d504a184531f111c3b36ad19905c9bee8738
nabom921126 <nabom921126@gmail.com>
2023-02-28 19:22:14 +0900

    first commit

---
+++ backend/src/main/java/com/finalproject/backend/TestController.java
@@ -0,0 +1,23 @@
+package com.finalproject.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+    
+    @RequestMapping(value = "/page1", method = RequestMethod.GET)
+    public String page1(){
+
+        return "page1";
+    }
+
+    @RequestMapping(value = "/page2", method = RequestMethod.GET)
+    public String page2(){
+
+        return "page2";
+    }
+
+
+}
