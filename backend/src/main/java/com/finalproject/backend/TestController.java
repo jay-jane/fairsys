118fdedcea424819ea1d571baf3a5c816fcbb760
sk223r5 <sk223r5@gmail.com>
2023-03-02 10:18:52 +0900

    20230302 10:18 page7 8 jy

--- backend/src/main/java/com/finalproject/backend/TestController.java
+++ backend/src/main/java/com/finalproject/backend/TestController.java
@@ -1,6 +1,7 @@
 package com.finalproject.backend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +19,4 @@ public class TestController {
 
         return "page2";
     }
-
-
 }
--- frontend/src/pages/page8/App.vue
+++ frontend/src/pages/page8/App.vue
@@ -1,8 +1,5 @@
-
 <template>
-
-  <h3>화면8. 이곳에서 html 작업을 진행하면 됩니다.</h3>
-
+  <h3>8번</h3>
 </template>
 
 <script>
