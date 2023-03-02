c2e2734ff7b4d5604864b58f32db948f78f17cbe
jaehaan <dksdbtjd4@naver.com>
2023-03-02 10:48:54 +0900

    20230302 page 3,4, 99 jaeha commit

--- frontend/src/pages/page99/App.vue
+++ frontend/src/pages/page99/App.vue
@@ -1,20 +1,20 @@
 
 <template>
 
-<!--공고페이지--> 
 
+<!--지원자목록-->
   <header>
       
-    <div class="menu1" style="border-bottom: 1px solid" >
+    <div class="menu1" style="border-bottom: 1px solid">
           <img src="#" style="float:left">
             <ul>
-              <li><a href="#">공고 </a></li>
-              <li><a href="#">업체</a></li>
-              <li><a href="#">통계</a></li>              
+              <li><a href="#">공고</a></li>
+              <li><a href="#">업체</a></li>              
+              <li><a href="#">통계</a></li>
               <li><a href="#">Q&A</a></li>  
-              <button style="float:right">로그아웃</button>     
+              <button style="float:right">로그아웃</button>       
             </ul>
-          
+        
       </div>
   </header> 
     <section>
@@ -23,24 +23,23 @@
         <table border="1" width="500">
         <tr>
             <td>번호</td>
-            <td>회사이름</td>
-            <td>모집제목</td>
-            <td>시작일</td>
-            <td>마감일</td>
+            <td>이름</td>
+            <td>공고제목</td>
+            <td>등록일</td>
+ 
         </tr>
         <tr>
-            <td>wwww</td>
-            <td>www</td>
-            <td>wwww</td>
-            <td>wwww</td>
-            <td>www</td>
+            <td>ㅌㅌㅌㅌ</td>
+            <td>ㅌㅌㅌ</td>
+            <td>ㅌㅌㅌ</td>
+            <td>ㅌㅌㅌ</td>
+  
         </tr>
         <tr>
-            <td>wwww</td>
-            <td>www</td>
-            <td>wwww</td>
-            <td>wwww</td>
-            <td>www</td>
+            <td>ㅌㅌㅌㅌ</td>
+            <td>ㅌㅌㅌㅌ</td>
+            <td>ㅌㅌㅌ</td>
+            <td>ㅌㅌㅌ</td>  
         </tr>
     </table>
  
@@ -53,17 +52,7 @@
 
 <footer style="border-top: 1px solid">
 
-ㅇ
-ㅇ
-ㅇ
-ㅇ
-ㅇ
-ㅇ
-ㅇ
-ㅇ
 
-ㅇ
-ㄴ
 내용아직안넣음
 
 
@@ -108,8 +97,6 @@ width: "100%";
 border: 1px solid #444444;
   
 }
-
-
 
 
   ul{
