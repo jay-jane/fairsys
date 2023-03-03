ff14aa06db592ed313583f3669b868a1be02af3c
leolee <srlee3637@naver.com>
2023-03-03 10:21:21 +0900

    2023.03.03 10:21 sr commit

--- frontend/src/pages/page19/App.vue
+++ frontend/src/pages/page19/App.vue
@@ -1,12 +1,13 @@
 
-
+<!-- 이력서 수정페이지 -->
 <template>
   <!-- 여기는 상단바 -->
   <header id="header">
 
-
+    
     <!-- 메뉴바 -->
     <div id="menu">
+      
       <ul>
         <!-- 로고 -->
         <li><img src="#"></li>
@@ -14,26 +15,65 @@
         <li><a href="page15">기업/연봉</a></li>
         <li><a href="page12">마이페이지</a></li>
         <li><a href="page2">로그인</a></li>
-      </ul>
+      </ul> 
     </div>
-
+      
     <!-- 로그인버튼 -->
 
   </header>
 
 
   <section id="section">
+  
+  <h3>인적사항</h3>
+  <input type="text" name="name"  placeholder="이름" >
+  <input type="text" name="birth"  placeholder="생년월일" >
+  <select>
+    <option value="">성별</option>
+    <option value="남">남</option>
+    <option value="여">여</option>
+  </select>
+  <input type="email" name="email"  placeholder="email" ><br>
+  
+  <input type="text" name="hp_number"  placeholder="hp_number">
+  <input type="text" name="adress"  placeholder="adress" ><br>
+  
+  <br><hr>
+  
+  <h3>희망직무</h3>
+  <input type="text" name="task"  placeholder="직무" ><br>
+  
+  <br><hr>
+  
+  <h3>자격증/어학</h3>
+  <input type="text" name="license"  placeholder="자격증" ><br>
+  
+  <input type="text" name="language"  placeholder="어학" ><br>
+  <br><hr>
+  
+  
+  
+  <h3>학력</h3>
+  <input type="text" name="education"  placeholder="학력" ><br>
+  
+  <br><hr>
+  
+  <h3>희망 근무조건</h3>
+  <input type="text" name="salary"  placeholder="희망연봉" ><br>
+  
+  <input type="text" name="language"  placeholder="어학" ><br>
+  <br><hr>
+  
 
-   <h3>page19</h3>
-
- 
   </section>
+
 
   <!-- 여기는 하단바 -->
   <footer id="footer">
     저작권 정보?
 
   </footer>
+
 </template>
 
 <script>
@@ -44,33 +84,26 @@ export default {
 </script>
 
 <style>
-* {
+*{
   margin: 0;
   padding: 0;
   list-style: none;
   text-align: center;
 }
 
-#header {}
+#header{
+  
+}
 
-#menu li {
+#menu li{
   display: inline;
   padding-left: 20px;
 }
 
 
 
-#section_menu {
-  display: flex;
-  justify-content: center;
-}
 
-tr{
-  display: flex;
-  justify-content: center;
-}
-
-#footer {
+#footer{
   height: 200px;
 }
 </style>
