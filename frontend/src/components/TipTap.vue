385e52a38e385c09885064cb4dacbc0ce46350db
sk223r5 <sk223r5@gmail.com>
2023-03-03 10:14:30 +0900

    20230303 10:14 tiptap jy

--- frontend/src/components/TipTap.vue
+++ frontend/src/components/TipTap.vue
@@ -1,33 +1,35 @@
 <template>
-    <editor-content :editor="editor" />
-  </template>
+  <editor-menu-bar :editor="editor" />
+  <editor-content :editor="editor" />
+</template>
   
-  <script>
-  import { Editor, EditorContent } from '@tiptap/vue-3'
-  import StarterKit from '@tiptap/starter-kit'
-  
-  export default {
-    components: {
-      EditorContent,
-    },
-  
-    data() {
-      return {
-        editor: null,
-      }
-    },
-  
-    mounted() {
-      this.editor = new Editor({
-        content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
-        extensions: [
-          StarterKit,
-        ],
-      })
-    },
-  
-    beforeUnmount() {
-      this.editor.destroy()
-    },
-  }
-  </script>
+<script>
+import { Editor, EditorContent, EditorMenuBar } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+  components: {
+    EditorMenuBar,
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null
+    }
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+      extensions: [
+        StarterKit,
+      ],
+    })
+  },
+
+  beforeUnmount() {
+    this.editor.destroy()
+  },
+}
+</script>
