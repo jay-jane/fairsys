24bce98ed65cb382e26b589d60ff1c162a7a2983
nabom921126 <nabom921126@gmail.com>
2023-03-07 10:15:10 +0900

    2023.03.06 작업물

--- frontend/src/pages/page43/App.vue
+++ frontend/src/pages/page43/App.vue
@@ -1,104 +1,33 @@
-<!-- 관리자페이지 ui -->
+
 <template>
-  <!-- <div class="admin_top_menu">
-    <div class="admin_account">
-      <button class="logout">로그아웃</button>
-    </div>
-  </div>
-
-  <ul class="admin_nav">
-    <hr/>
-    <li class="sub_menu_toggle">
-      <a class="sub menu select">기업</a>
-      <hr/>
-      <ul class="sub_menu" style="display: block;">
-        <li><a href="#">기업 목록</a></li>
-        <li><a href="#">기업 신청 관리</a></li>
-      </ul>
-    </li>
-    <hr/>
-
-    <li class="sub_menu_toggle">
-      <a href="#">유저</a>
-      <hr/>
-      <ul class="sub_menu">
-        <li><a href="#">유저 목록</a></li>
-        <li><a href="#">유저 관리</a></li>
-      </ul>
-    </li>
-    <hr/>
-
-    <li class="sub_menu_toggle">
-      <a href="#">통계</a>
-      <hr/>
-      <ul class="sub_menu">
-        <li><a href="#">기업 통계</a></li>
-        <li><a href="#">유저 통계</a></li>
-      </ul>
-    </li>
-    <hr/>
-    
-    <li class="sub_menu_toggle">
-      <a href="#">Q&A</a>
-      <hr/>
-      <ul class="sub_menu">
-        <li><a href="#">Q&A 등록</a></li>
-        <li><a href="#">Q$A 관리</a></li>
-      </ul>
-    </li>
-    <hr/>
-
-    </ul> -->
-
-  <div class="admin_top">
-    <div class="admin_logo">
-      <a href="#">홈으로</a>
-    </div>
-    <div class="admin_account">
-      <span>관리자님 환영합니다!</span>
-      <button @click="doLogout">로그아웃</button>
-    </div>
-  </div>
-
-  <div class="admin_nav">
-    <ul>
-      <li class="sub_menu_toggle">
-        <a class="sub menu select">기업</a>
-        <ul class="sub_menu" style="display: block;">
-          <li><a href="#">기업 목록</a></li>
-          <li><a href="#">기업 신청 관리</a></li>
-        </ul>
-      </li>
-
-      <li class="sub_menu_toggle">
-        <a href="#">유저</a>
-        <ul class="sub_menu">
-          <li><a href="#">유저 목록</a></li>
-          <li><a href="#">유저 관리</a></li>
-        </ul>
-      </li>
-
-      <li class="sub_menu_toggle">
-        <a href="#">통계</a>
-        <ul class="sub_menu">
-          <li><a href="#">기업 통계</a></li>
-          <li><a href="#">유저 통계</a></li>
-        </ul>
-      </li>
-
-      <li class="sub_menu_toggle">
-        <a href="#">Q&A</a>
-        <ul class="sub_menu">
-          <li><a href="#">Q&A 등록</a></li>
-          <li><a href="#">Q$A 관리</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
-
+ 
   <div class="content">
-    d안녕안녕
+
+    <h1>Q&A 등록</h1>
+    <div class="table_border">
+      <table class="registration">
+        <tr>
+          <td>제목</td>
+          <td><input type="text" name="" id="" style="width: 870px; height: 50px;" ></td>
+        </tr>
+  
+        <tr>
+          <td>내용</td>
+          <td><textarea name="" id="" class="input-style" ></textarea></td>
+        </tr>
+  
+        <tr>
+          <td></td>
+          <td>
+            <input type="submit" value="등록하기" class="btn btn-primary">
+            <button class="btn btn-secondary" @click="doCancel">취소하기</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+
   </div>
+
 </template>
 
 <script>
@@ -108,6 +37,9 @@ export default {
   methods:{
     doLogout(){
       location.href ="page2";
+    },
+    doCancel(){
+      location.href = "#";
     }
   }
 }
@@ -117,67 +49,75 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Jua&family=Noto+Sans+KR:wght@100;300;400;500;700&display=swap');
 
 * {margin: 0; padding: 0; font-family: 'Noto Sans KR', sans-serif; text-decoration: none; color: black;}
-
-.admin_top {
-  background-color: #263238;
-  height: 100px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.admin_top a {
-  color: orange;
-  text-decoration: none;
-}
-
-.admin_logo {
-  margin-left: 20px;
-}
-
-.admin_account {
-  margin-right: 20px;
-}
-
-.admin_account span{
-  color: white;
-  margin-right: 20px;
-}
-
-.admin_nav {
-  width: 200px;
-  height: 100%;
-  position: fixed;
-  top: 100px;
-  left: 0;
-  background-color: #263238;
-}
-
-  .admin_nav li{
-    margin-top: 20px;
-    list-style: none;
-  }
-
-  .admin_nav li a {
-      padding: 8px 20px;
-      text-decoration: none;
-      font-size: 15px;
-      color: orange;
-      display: block;
-      width: 200px;
-      font-weight: 600;
-  }
-
-  .admin_nav .sub_menu {margin-left: 25px;}
-  .admin_nav .sub_menu li a {color: #fff; display: block; width: 155px; padding: 8px 10px 8px 10px; }
-  .admin_nav .sub_menu li a:hover { background-color: orange;}
-  .admin_nav .sub_menu li a.on {background-color: orange;}  
   
-  .content {
-  margin-left: 200px; /* admin_nav의 너비 + 20px 마진을 주어서 겹치지 않도록 함 */
-  background-color: aqua;
+.content {
   height: 100%;
-  }
+}
+
+.content h1 {
+  margin: 20px auto;
+  padding: 8px 20px;
+}
+
+.table_border {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+  margin: 0 auto;
+  width: 75%;
+  padding: 20px;
+}
+
+.registration {
+  margin: 0 auto;
+  font-size: 16px;
+}
+
+.registration td {
+  padding: 10px;
+  text-align: center;
+}
+
+.input-style {
+  width: 850px;
+  height: 400px;
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  resize: none;
+  font-size: 16px;
+}
+
+.btn {
+  padding: 10px 20px;
+  border-radius: 3px;
+  font-size: 16px;
+  font-weight: 500;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+
+.btn-primary {
+  background-color: orange;
+  color: #fff;
+  border: none;
+  margin-right: 10px;
+}
+
+.btn-primary:hover {
+  background-color: orangered;
+}
+
+.btn-secondary {
+  background-color: #f8f9fa;
+  color: #212529;
+  border: 1px solid #ccc;
+}
+
+.btn-secondary:hover {
+  background-color: #e9ecef;
+  border-color: #adb5bd;
+}
 
 </style>
 
