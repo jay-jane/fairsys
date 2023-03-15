2932b72b23383f2be012857926c26517be58d121
nabom921126 <nabom921126@gmail.com>
2023-03-15 14:06:30 +0900

    2023.03.15 14:06 오하영

--- backend/src/main/java/com/finalpj/backend/service/EmploymentQnaService.java
+++ backend/src/main/java/com/finalpj/backend/service/EmploymentQnaService.java
@@ -3,7 +3,6 @@ package com.finalpj.backend.service;
 import java.util.ArrayList;
 
 import com.finalpj.backend.command.EmploymentQnaVO;
-import com.finalpj.backend.command.UserVO;
 import com.finalpj.backend.util.Criteria;
 
 public interface EmploymentQnaService {
@@ -13,4 +12,7 @@ public interface EmploymentQnaService {
 
     //페이지네이션용
     public int getTotal(String user_id, Criteria cri);
+
+    //게시글 상세내용
+    public ArrayList<EmploymentQnaVO> getDetail(int qa_no, String user_id);
 }
