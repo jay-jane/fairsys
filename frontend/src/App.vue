de23b9f7bf23f19e65ee15df82ac00a026286914
leolee <srlee3637@naver.com>
2023-03-21 15:16:04 +0900

    20230321 15:15 sr commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <Header></Header>
-  
+  <UserHeader v-if="ut_no == 1 || ut_no == null" class="userHeader"  :key="ut_no"></UserHeader>
+  <CompanyHeader v-if="ut_no == 2" class="companyHeader" :key="ut_no"></CompanyHeader>
   <!-- <AdminHeader class="name"></AdminHeader> -->
   <Side></Side>
   <router-view />
@@ -8,25 +8,62 @@
 </template>
 
 <script>
-import Header from './layouts/Header.vue';
+import UserHeader from './layouts/UserHeader.vue';
+import CompanyHeader from './layouts/CompanyHeader.vue';
 import Footer from './layouts/Footer.vue';
 import Side from './layouts/Side.vue';
 import AdminHeader from './layouts/AdminHeader.vue';
 
 
+
 export default {
   components: {
-    Header,
+    UserHeader,
+    CompanyHeader,
     AdminHeader,
     Footer,
     Side,
-  }
+  },
+  data() {
+    return {
+      user_id: '',
+      user_pw: '',
+      com_id: '',
+      com_pw: '',
+      ut_no: this.$store.state.ut_no,
+    }
+  },
+  methods: {
+    checkUser() {
+      const sessionUtNo = sessionStorage.getItem("ut_no")
+      console.log("sessionUtNo:" + sessionUtNo)
+      if (sessionUtNo == 1 || sessionUtNo == null) {
+        this.$store.commit("setUtNo", 1)
+        this.ut_no = this.$store.state.ut_no
+        console.log(this.ut_no)
+      } else {
+        this.$store.commit("setUtNo", 2)
+        this.ut_no = this.$store.state.ut_no
+        console.log(this.ut_no)
+      }
+    },
+  },
+  created() {
+    this.checkUser()
+  },
+  watch: {
+    'ut_no': function () {
+      console.log(this.ut_no)
+      this.checkUser();
+    }
+  },
+
 }
 
 </script>
 
 <style>
-.name{
+.name {
   display: none;
 }
 </style>
